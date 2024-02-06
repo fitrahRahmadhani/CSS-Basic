b8beb875dcd241a244ccc0223f83d4e422ab8c80
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-02-06 08:32:28 +0700

    belajar tailwindcss WPU

---
+++ WPU/TAILWIND/basic-utility/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      spacing:{
+        13: '3.25rem',
+      },
+      fontFamily:{
+        inter: ['Inter']
+      },
+      colors:{
+        wpu: '#bada55'
+      }
+    },
+  },
+  plugins: [],
+}
+
