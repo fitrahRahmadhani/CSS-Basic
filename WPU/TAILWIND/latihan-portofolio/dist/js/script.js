53cfed9dcd59e4398fd2f4b2aa5fb69950e5bcf1
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-02-09 18:57:14 +0700

    latihan tailwindcss WPU

---
+++ WPU/TAILWIND/latihan-portofolio/dist/js/script.js
@@ -0,0 +1,16 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu')
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    if(window.scrollY > fixedNav){
+        header.classList.add('navbar-fixed');
+    }else{
+        header.classList.remove('navbar-fixed');
+    }
+}
