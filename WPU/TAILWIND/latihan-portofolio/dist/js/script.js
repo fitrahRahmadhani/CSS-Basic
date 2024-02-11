41e1abdb40d766f561dc4efae2dfcf213606e546
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-02-11 07:30:37 +0700

    latihan tailwindcss WPU

--- WPU/TAILWIND/latihan-portofolio/dist/js/script.js
+++ WPU/TAILWIND/latihan-portofolio/dist/js/script.js
@@ -1,5 +1,9 @@
 const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu')
+const navMenu = document.getElementById('nav-menu');
+const backToTop = document.getElementById('back-to-top');
+const darkToogle = document.getElementById('dark-toggle');
+const html = document.querySelector('html');
+
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
@@ -10,7 +14,15 @@ window.onscroll = function(){
     const fixedNav = header.offsetTop;
     if(window.scrollY > fixedNav){
         header.classList.add('navbar-fixed');
+        backToTop.classList.add('flex');
+        backToTop.classList.remove('hidden');
     }else{
         header.classList.remove('navbar-fixed');
+        backToTop.classList.remove('flex');
+        backToTop.classList.add('hidden');
     }
 }
+darkToogle.addEventListener('click', function(){
+    darkToogle.checked ? html.classList.add('dark') : html.classList.remove('dark');
+})
+
